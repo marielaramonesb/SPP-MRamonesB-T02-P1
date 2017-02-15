96d3e80e2d800de61615e5efac8d6d8b287af0e2
marielaramonesbalvoa <marielaramonesbalvoa@10.34.157.204>
2017-02-14 23:20:12 -0600

    CALCULADOR SALARIO, HORAS EXTRA

--- src/spp/mramonesb/t02/SPPMRamonesBT02.java
+++ src/spp/mramonesb/t02/SPPMRamonesBT02.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Mariela Ramones Balvoa- A01411449- IMT
 package spp.mramonesb.t02;
 import java.util.Scanner;
 /**
